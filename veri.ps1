1cc4f9bd1815eb648e8d9f5ece3053f794bcc9dd
tay.io <128968711+berlintay@users.noreply.github.com>
2024-02-03 07:17:46 -0400

    Update veri.ps1
    hopefully this works :-)

--- veri.ps1
+++ veri.ps1
@@ -1,28 +1,22 @@
 function Update-WingetV {
     param (
-        [string]$Url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+        [string]$URL = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
     )
+  
 
- 
-    $latestRelease = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
-        Select-Object -ExpandProperty "assets" |
-        Where-Object "browser_download_url" -Match '.msixbundle' |
-        Select-Object -ExpandProperty "browser_download_url"
-    $latestVersion = $latestRelease.tag_name
-    $userHomeDirectory = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
-    $CurrentVersion = winget --Version
+  $CurrentVersion = winget --Version
+  $latestversion = Invoke-RestMethod -Uri $URL).Tag_name
     if ($CurrentVersion -ge $latestVersion) {
         Write-Host "Winget is up to date, Version is $CurrentVersion"
     } else {
         Write-Host "winget needs to update before running the script. Installing latest version..."
 
-        $downloadUrl = $latestRelease.assets[0].browser_download_url
-
-        $wingetDownloadPath = "$userHomeDirectory"
+        $wingetDownloadPath = "$env:TEMP\winget.msixbundle" 
 
         Write-Host "Donwloading latest winget-cli version ..."
-        Invoke-RestMethod -Uri $downloadUrl -OutFile $wingetDownloadPath
-        Hide-Spinner
+        $downloadUrl  = (Invoke-RestMethod -Uri $URL).assets;Where-Object { $_. browser_download_url -Match '.msixbundle' };Select-Object -ExpandProperty browser_download_url
+        Invoke-WebRequest -Uri $ downloadUrl -Outfile $wingetDownloadPath
+
 
         Write-Host "Installing now"
         Invoke-Expression -Command "Add-AppxPackage -Path $wingetDownloadPath"
@@ -30,18 +24,8 @@ function Update-WingetV {
     }
 }
 
-
-
-
-
-
-$GitHubRepoOwner = "microsoft"
-$GitHubRepoName = "winget-cli"
-
-Update-WingetV -GitHubRepoOwner $GitHubRepoOwner -GitHubRepoName $GitHubRepoName
-
-Write-Host "Refreshing Env"
-Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
-RefreshEnv
+# Write-Host "Refreshing Env"
+# Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+# RefreshEnv
 
 Invoke-WebRequest -Uri https://christitus.com/win | Invoke-Expression
