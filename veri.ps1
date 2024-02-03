e40c363c9b0909e6215a571868921990cda60ce4
tay.io <128968711+berlintay@users.noreply.github.com>
2024-02-03 01:15:46 -0400

    tester

---
+++ veri.ps1
@@ -0,0 +1,50 @@
+function Update-WingetV {
+    param (
+        [string]$Url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+    )
+
+    $CurrentVersion = winget --Version
+    $latestRelease = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
+        Select-Object -ExpandProperty "assets" |
+        Where-Object "browser_download_url" -Match '.msixbundle' |
+        Select-Object -ExpandProperty "browser_download_url"
+    $latestVersion = $latestRelease.tag_name
+    $userHomeDirectory = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
+    $
+    if ($CurrentVersion -ge $latestVersion) {
+        Write-Host "Winget is up to date, Version is $CurrentVersion"
+    } else {
+        Write-Host "winget needs to update before running the script. Installing latest version..."
+
+        $downloadUrl = $latestRelease.assets[0].browser_download_url
+
+        $wingetDownloadPath = "$userHomeDirectory"
+
+        Write-Host "Donwloading latest winget-cli version ..."
+        Invoke-RestMethod -Uri $downloadUrl -OutFile $wingetDownloadPath
+        Hide-Spinner
+
+        Write-Host "Installing now"
+        Invoke-Expression -Command "Add-AppxPackage -Path $wingetDownloadPath"
+        Write-Host "winget updated to latest version"
+    }
+}
+
+function Update-VC {
+    [string]$checkvc = winget -show Microsoft.VCRedist.2015+.x64
+
+    if ($checkvc -ge $checkvc)
+
+}
+
+
+$GitHubRepoOwner = "microsoft"
+$GitHubRepoName = "winget-cli"
+
+Update-WingetV -GitHubRepoOwner $GitHubRepoOwner -GitHubRepoName $GitHubRepoName
+
+Write-Host "Refreshing Env"
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+RefreshEnv
+
+Invoke-WebRequest -Uri https://christitus.com/win | Invoke-Expression
